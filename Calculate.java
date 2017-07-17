7f92a13700c014a097389645f4dfe434e346aaaa
unknown <mimbletoniya@mail.ru>
2017-07-17 11:11:45 +0300

    some calculations

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,18 @@
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+		double first = Double.valueOf(args[0]);
+		double second = Double.valueOf(args[1]);
+		double sum, subtract, multiplication, division, power;
+		sum = first + second;
+		subtract = first - second;
+		multiplication = first * second;
+		division = first / second;
+		power = Math.pow(first, second);
+		System.out.println("Sum = "+sum);
+		System.out.println("Subtract = "+subtract);
+		System.out.println("Multiplication = "+multiplication);
+		System.out.println("Division = "+division);
+		System.out.println("Power = "+power);
 	}
 }
